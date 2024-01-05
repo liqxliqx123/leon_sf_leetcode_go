75b76f77a96e7eb5555cd990c7cae45d8c779949
leonlibj <leonlibj@wisers.com>
2024-01-05 10:19:56 +0800

    1

--- arrary/easy/array_test.go
+++ arrary/easy/array_test.go
@@ -145,4 +145,24 @@ func containsNearbyDuplicateV2(nums []int, k int) bool {
 	return false
 }
 
-// 0 1 2 3 4 5
+// 丢失的数字
+// https://leetcode.cn/problems/missing-number/
+func TestMissingNumber(t *testing.T) {
+	fmt.Println(missingNumber([]int{3, 0, 1}))
+	fmt.Println(missingNumber([]int{0, 1}))
+	fmt.Println(missingNumber([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
+	fmt.Println(missingNumber([]int{0}))
+}
+
+func missingNumber(nums []int) int {
+	//排序
+	sort.Ints(nums)
+	//遍历数组
+	for i := 0; i < len(nums); i++ {
+		//判断是否连续
+		if nums[i] != i {
+			return i
+		}
+	}
+	return len(nums)
+}
